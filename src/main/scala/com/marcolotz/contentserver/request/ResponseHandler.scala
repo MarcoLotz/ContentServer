97f730c3a08325c3134ea3a9c8c9c2877d167294
Marco Aurelio Lotz <lotzutfpr@gmail.com>
2017-09-12 16:00:49 +0200

    fixed wrong content type respose

--- src/main/scala/com/marcolotz/contentserver/request/ResponseHandler.scala
+++ src/main/scala/com/marcolotz/contentserver/request/ResponseHandler.scala
@@ -2,7 +2,9 @@ package com.marcolotz.contentserver.request
 
 import java.io.File
 import java.util.Properties
+import javax.servlet.http.HttpServletRequest
 
+import com.typesafe.scalalogging.LazyLogging
 import org.apache.commons.io.FilenameUtils
 
 import scala.io.Source
@@ -10,10 +12,12 @@ import scala.io.Source
 /**
   * Created by Marco Lotz on 08/06/2017.
   */
-object ResponseHandler {
+object ResponseHandler extends LazyLogging {
 
+  // Load properties lookup file
   private val properties: Properties = new Properties()
-  Source.fromInputStream(getClass().getClassLoader().getResourceAsStream("extensions.properties"))
+  val reader = Source.fromURL(getClass.getResource("/extensions.properties")).bufferedReader()
+  properties.load(reader)
 
   /** *
     * Resolves the response header content type based on the file extension
@@ -23,13 +27,29 @@ object ResponseHandler {
     */
   def resolveContentType(resourcePath: String): String = {
     val file = new File(resourcePath)
-     if (file.isDirectory) "application/zip"
-     else {
-     val extension = Option(properties.get(FilenameUtils.getExtension(resourcePath).toLowerCase()))
-       extension match {
-         case Some(ex) => ex.toString
-         case None => "application/octet-stream"
-       }
-     }
+    if (file.isDirectory) "application/zip"
+    else {
+      val extension = Option(properties.get(FilenameUtils.getExtension(resourcePath).toLowerCase()))
+      extension match {
+        case Some(ex) => ex.toString
+        case None => "application/octet-stream"
+      }
+    }
+  }
+
+  /**
+    * Resolves the content type of requests sent at the endpoint without defined servlets
+    *
+    * @param request
+    * @return content type
+    */
+  def resolveDefaultRequestResponse(request: HttpServletRequest): String = {
+    val extension = FilenameUtils.getExtension(request.getRequestURL.toString)
+    val foundType = Option(properties.get(extension)) match {
+      case Some(ex) => ex.toString
+      case None => "text/html"
+    }
+    logger.debug("resolved for request at " + request.getRequestURL + " is type: " + foundType)
+    foundType
   }
 }
--- src/main/scala/com/marcolotz/contentserver/servlets/LandingServlet.scala
+++ src/main/scala/com/marcolotz/contentserver/servlets/LandingServlet.scala
@@ -2,12 +2,18 @@ package com.marcolotz.contentserver.servlets
 
 import com.marcolotz.contentserver.auth.AuthenticationSupport
 import com.marcolotz.contentserver.renderer.Renderer
+import com.marcolotz.contentserver.request.ResponseHandler
+import com.typesafe.scalalogging.LazyLogging
 import org.scalatra.ScalatraServlet
 
-class LandingServlet extends ScalatraServlet with AuthenticationSupport {
+class LandingServlet extends ScalatraServlet with AuthenticationSupport with LazyLogging{
 
+  /** *
+    * Custom implemented to handle css and javascript objects
+    */
+  // TODO: make this also for video
   before() {
-    contentType = "text/html"
+    contentType = ResponseHandler.resolveDefaultRequestResponse(request)
   }
 
   get("/") {
